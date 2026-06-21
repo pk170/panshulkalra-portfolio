fb587c2ebcffa36649cb7fdb4b87c75c4bacc6e9
Panshul Kalra <panshulkalra@gmail.com>
2026-06-21 12:49:45 +0530

    frontend

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { Layout, Hero, About, Blog, Contact } from '@components';
+import Projects from '../components/sections/projects';
 
 const StyledMainContainer = styled.main`
   counter-reset: section;
@@ -13,6 +14,7 @@ const IndexPage = ({ location }) => (
       <Hero />
       <About />
       <Blog />
+      <Projects />
       <Contact />
     </StyledMainContainer>
   </Layout>
